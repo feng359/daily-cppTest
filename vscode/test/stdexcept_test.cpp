d5864228cd5037b7daf4b545c98fa16af006b559
feng <810998641@qq.coM>
2022-07-22 17:47:55 +0800

    异常

--- vscode/test/stdexcept_test.cpp
+++ vscode/test/stdexcept_test.cpp
@@ -12,7 +12,7 @@ int main()
     int b=3;
     try{
  
-      //通过以下测试，throw可以抛出const char* 、类、int型等，但要注意，throw抛出的类型一定要和catch捕捉的参数类型一致；
+      //通过以下测试，throw可以抛出const char* 、对象、int型等，但要注意，throw抛出的类型一定要和catch捕捉的参数类型一致；
       //try抛出异常后，catch捕捉异常，从第一个catch一直找到对应的抛出的异常类型，执行里面的代码。
       //if (a>b) throw "比较错误";
 
